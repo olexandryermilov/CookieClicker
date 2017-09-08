7b6271703d3c120c52d43e0bdd30f726dc3bbb14
Olexandr Yermilov <olexandr.yermilov@gmail.com>
2017-09-09 01:41:15 +0300

    //added cursor class

--- src/main/java/Building.java
+++ src/main/java/Building.java
@@ -4,8 +4,40 @@ import java.util.ArrayList;
  * Created by Олександр on 09.09.2017.
  */
 public abstract class Building {
-     protected int cost;
-     protected int amount;
-     protected int cookiesPerSecond;
-     protected ArrayList<Upgrade> upgrades;
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getCookiesPerSecond() {
+        return cookiesPerSecond;
+    }
+
+    public void setCookiesPerSecond(int cookiesPerSecond) {
+        this.cookiesPerSecond = cookiesPerSecond;
+    }
+
+    public ArrayList<Upgrade> getUpgrades() {
+        return upgrades;
+    }
+
+    public void setUpgrades(ArrayList<Upgrade> upgrades) {
+        this.upgrades = upgrades;
+    }
+
+    protected int cost;
+    protected int amount;
+    protected int cookiesPerSecond;
+    protected ArrayList<Upgrade> upgrades;
 }
--- src/main/java/Cursor.java
+++ src/main/java/Cursor.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 /**
  * Created by Олександр on 09.09.2017.
  */
-public class Cursor {
+public class Cursor extends Building{
+    public Cursor()
+    {
+        super();
+        cost = 10;
+        cookiesPerSecond=15;
+        upgrades= new ArrayList<Upgrade>();
+        //todo : add upgrades
+        amount=0;
+    }
+
 }
