bc10a41818979fef89d6886f23f37bc8da0ac836
Olexandr Yermilov <olexandr.yermilov@gmail.com>
2017-09-09 01:50:08 +0300

    //refactored Cursor

--- src/main/java/Cursor.java
+++ src/main/java/Cursor.java
@@ -4,14 +4,17 @@ import java.util.ArrayList;
  * Created by Олександр on 09.09.2017.
  */
 public class Cursor extends Building{
-    public Cursor()
-    {
+    private static final Cursor INSTANCE  = new Cursor();
+    private Cursor() {
         super();
         cost = 10;
-        cookiesPerSecond=15;
-        upgrades= new ArrayList<Upgrade>();
+        cookiesPerSecond = 15;
+        upgrades = new ArrayList<Upgrade>();
         //todo : add upgrades
-        amount=0;
+        amount = 0;
     }
-
+    public static Cursor getInstance()
+    {
+        return INSTANCE;
+    }
 }
